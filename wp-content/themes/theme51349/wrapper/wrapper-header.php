d61351b18d730d14b4a4da38c4a7c2bdc1ea9902
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-02-27 14:18:10 -0800

    head rev reg widtsh

--- wp-content/themes/theme51349/wrapper/wrapper-header.php
+++ wp-content/themes/theme51349/wrapper/wrapper-header.php
@@ -10,10 +10,10 @@
 <div class="row">
 	<div class="span12 nav-wrap">
 		<div class="row">
-			<div class="span10" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
+			<div class="span9" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
 				<?php get_template_part("static/static-nav"); ?>
 			</div>
-			<div class="span2 hidden-phone" data-motopress-type="static" data-motopress-static-file="static/static-search.php">
+			<div class="span3 hidden-phone" data-motopress-type="static" data-motopress-static-file="static/static-search.php">
 				<?php get_template_part("static/static-search"); ?>
 			</div>
 		</div>
