1223e5a1c56a16062070516052c6c89b146a9602
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-02-26 22:07:54 -0800

    newer ups

---
+++ wp-content/themes/theme51349/wrapper/wrapper-header.php
@@ -0,0 +1,21 @@
+<?php /* Wrapper Name: Header */ ?>
+<div class="row">
+	<div class="span6" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
+		<?php get_template_part("static/static-logo"); ?>
+	</div>
+	<div class="span6 social-nets-wrapper" data-motopress-type="static" data-motopress-static-file="static/static-social-networks.php">
+		<?php get_template_part("static/static-social-networks"); ?>
+	</div>
+</div>
+<div class="row">
+	<div class="span12 nav-wrap">
+		<div class="row">
+			<div class="span9" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
+				<?php get_template_part("static/static-nav"); ?>
+			</div>
+			<div class="span3 hidden-phone" data-motopress-type="static" data-motopress-static-file="static/static-search.php">
+				<?php get_template_part("static/static-search"); ?>
+			</div>
+		</div>
+	</div>
+</div>
