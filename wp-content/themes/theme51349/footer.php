847bdd99df6b6eb9fac62a0130a8a36eb1e748fc
Jason Raby <jason@articulate-solutions.com>
2017-05-19 20:28:21 +0000

    sponsor tracking

--- wp-content/themes/theme51349/footer.php
+++ wp-content/themes/theme51349/footer.php
@@ -19,19 +19,20 @@
 			<?php echo stripslashes(of_get_option('ga_code')); ?>
 		</script>
 		<script>
-		/**
-		* Function that tracks a click on an outbound link in Analytics.
-		* This function takes a valid URL string as an argument, and uses that URL string
-		* as the event label. Setting the transport method to 'beacon' lets the hit be sent
-		* using 'navigator.sendBeacon' in browser that support it.
-		*/
-		var trackOutboundLink = function(url) {
-		   ga('send', 'event', 'outbound', 'click', url, {
-		     'transport': 'beacon',
-		     'hitCallback': function(){document.location = url;}
-		   });
+		ga('create', 'UA-42092269-1', 'gilroygarlicfestival.com');
+		ga('send', 'pageview');
+		function trackOutboundLink(link, category, action) { 
+		 
+			try { 
+				_gaq.push(['_trackEvent', category , action]); 
+			} catch(err){}
+			 
+			setTimeout(function() {
+			document.location.href = link.href;
+			}, 100);
 		}
 		</script>
+
 		<!-- Show Google Analytics -->
 	<?php } ?>
 	<?php wp_footer(); ?> <!-- this is used by many Wordpress features and for plugins to work properly -->
